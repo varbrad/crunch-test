93fd9d9ea192ee18258e88d9cbe8f7b1d1f00ccb
Brad Davies <me@varbrad.dev>
2020-06-23 21:13:26 +0100

    MVP

--- resources/js/component/thing.js
+++ resources/js/component/thing.js
@@ -1,1 +1,1 @@
-export default 'WOWasdasd!'
+export default 'Brad! qweis aasdsd, asdwayoo!!'
--- scripts/dev.js
+++ scripts/dev.js
@@ -1,10 +1,24 @@
+const program = require('commander')
 const path = require('path')
 const webpack = require('webpack')
 const WebpackDevServer = require('webpack-dev-server')
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const ora = require('ora')
+const chalk = require('chalk')
+
+program
+    .option('-p, --port <port>', 'The port to run the bundle server on', 8080)
+    .option('--path <path>', 'The path to build to', '/dist/')
+const args = program.parse(process.argv)
+
+const PATH = args.path
+const PORT = args.port
 
 const ENTRY = path.resolve(__dirname, '../resources/js/main.js')
-const DIST = path.join(__dirname, '../public/dist')
+const DIST = path.join(__dirname, '../public', PATH)
+
+const CONTENT_BASE = path.join(__dirname, '../public')
+const PUBLIC_PATH = `http://localhost:${PORT}${PATH}`
 
 const config = {
   mode: 'development',
@@ -29,19 +43,22 @@ const config = {
   ],
   output: {
     filename: '[name].js',
-    path: DIST,
-    publicPath: 'http://localhost:8080/dist/',
+    path: PATH,
+    publicPath: PUBLIC_PATH,
   },
 }
 
+const log = ora({ prefixText: chalk.cyan('Crunch'), text: 'Preparing to launch server...', spinner: 'bouncingBar' }).start()
+
 const compiler = webpack(config)
+
 const server = new WebpackDevServer(compiler, {
-    contentBase: path.join(__dirname, '../public'),
-    publicPath: 'http://localhost:8080/dist/',
+    contentBase: CONTENT_BASE,
+    publicPath: PUBLIC_PATH,
     hotOnly: true,
     open: false,
     stats: false,
-    port: 8080,
+    noInfo: true,
     disableHostCheck: true,
     headers: {
         "Access-Control-Allow-Origin": "*",
@@ -50,6 +67,21 @@ const server = new WebpackDevServer(compiler, {
     }
 })
 
-server.listen(8080, '127.0.0.1', () => {
-    console.log('Starting server on http://localhost:8080');
+
+compiler.hooks.beforeCompile.tap('Crunch', () => {
+    log.start('Crunching...')
+})
+
+compiler.hooks.afterCompile.tap('Crunch', (compilation) => {
+    const { errors } = compilation
+    if (errors && errors.length > 0) {
+        log.fail('An error occured!\n' + JSON.stringify(errors, null, 2))
+    } else {
+        log.succeed('Built!')
+    }
+    log.stopAndPersist({ symbol: '👀', text: 'Watching for changes...' })
+})
+
+server.listen(PORT, '127.0.0.1', () => {
+    log.succeed(`Dev server is up! - ${chalk.yellow(PUBLIC_PATH)}`)
 });
