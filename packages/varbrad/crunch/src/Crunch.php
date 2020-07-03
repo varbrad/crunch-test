3032bd55c0c03a2a6f515b95f79b51381398f324
Brad Davies <me@varbrad.dev>
2020-07-03 20:04:24 +0100

    Get crunch working

--- packages/varbrad/crunch/src/Crunch.php
+++ packages/varbrad/crunch/src/Crunch.php
@@ -2,10 +2,20 @@
 
 namespace Varbrad\Crunch;
 
+use Illuminate\Support\Facades\File;
+
 class Crunch
 {
-    public function asset(): bool
+    public static function entrypoint($name): string
     {
-        return true;
+        $path = storage_path('app/crunch.manifest.json');
+        $file = json_decode(File::get($path));
+        $publicPath = $file->publicPath;
+        $scripts = collect($file->entrypoints->$name);
+        return $scripts
+            ->map(function ($assetName) use ($publicPath) {
+                return '<script src="' . $publicPath . $assetName . '" defer></script>';
+            })
+            ->join('');
     }
 }
